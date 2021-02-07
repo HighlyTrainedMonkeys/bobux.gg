0d54c24524411d8a4c5ee90c2d912fa2fd2da2ea
grangus <31970023+grangus@users.noreply.github.com>
2021-02-07 15:41:43 -0600

    disabled origin check

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(helmet());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://bobux.gg");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
