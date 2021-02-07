7bccee236fcefb63c5be50d1cd4ed7165184434a
grangus <31970023+grangus@users.noreply.github.com>
2021-02-07 15:07:32 -0600

    annoying cors shit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(helmet());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "bobux.gg");
+  res.header("Access-Control-Allow-Origin", "https://bobux.gg");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
