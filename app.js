87efe387c3ac5a188350f9eaa6e4367a6754161e
grangus <31970023+grangus@users.noreply.github.com>
2021-02-08 17:51:43 -0600

    aaa

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const mongoose = require("mongoose");
 const ioredis = require("ioredis");
 const fs = require("fs");
 const admin = require("firebase-admin");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+
+Sentry.init({
+  dsn: "https://b527502e32dc4fdc83245f5fdd10a069@o435421.ingest.sentry.io/5626944",
+  tracesSampleRate: 1.0,
+});
 
 const serviceAccount = require("./fbkey.json");
 
