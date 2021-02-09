51038ab9054a9ea3b07b34a1352c5da26281fac9
grangus <31970023+grangus@users.noreply.github.com>
2021-02-08 18:32:40 -0600

    allowing username header

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
     "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Authentication"
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Authentication, Username"
   );
 
   next();
